83b574a3e1824c99fc712d886b93121ec163f691
Nico Limo <nicolas.limonoff@gmail.com>
2023-03-27 19:10:06 -0300

    added more regex expression and tests

--- index.js
+++ index.js
@@ -1,25 +1,107 @@
 // Ejercicios Regex
+
+// the i will ignore cases like
 const ignoreCase = (myString) => {
   const fccRegex = /freeCodeCamp/i
   return fccRegex.test(myString)
 }
+
+// match function will find the same word and return an array
 const match = (myString) => {
   const codingRegex = /coding/
-  let [stringResult] = myString.match(codingRegex)
+  const [stringResult] = myString.match(codingRegex)
   return stringResult
 }
+
 const matchAllCase = (myString) => {
   // You can put parameters together
-  let starRegex = /Twinkle/gi
+  const starRegex = /Twinkle/gi
   return myString.match(starRegex)
 }
 const matchDot = (myString) => {
-  let unRegex = /un./
+  const unRegex = /un./
   return unRegex.test(myString)
 }
 const vocalsMatch = (myString) => {
   const vowelRegex = /[aeiou]/gi
   return myString.match(vowelRegex)
+}
+const vocalsRangeMatch = (myString) => {
+  const myRegex = /[a-z]/gi
+  return myString.match(myRegex)
+}
+const rangeconsttersNumbersMatch = (myString) => {
+  const myRegex = /[h-s2-6]/gi
+  return myString.match(myRegex)
+}
+
+// ^ negate and find all the character not equal, inclusive spaces dots, !, [], or comas
+const rangeNegateMatch = (myString) => {
+  const myRegex = /[^aeiou0-9]/gi
+  return myString.match(myRegex)
+}
+
+// + Check if a constter is consecutire like match +a on aaba will return ['aa','a']
+const matchConsecutive = (myString) => {
+  const myRegex = /s+/g
+  return myString.match(myRegex)
+}
+
+// * Check if a constter occur zero or more time like match +a on aaba will return ['aa','a']
+const matchZeroOrMore = (myString) => {
+  const myRegex = /Aa*/
+  const result = myString.match(myRegex)
+  return result
+}
+
+// The dot is a wildcard and the ? will find the first match between the search parameter
+const lazyMatch = (myString) => {
+  const myRegex = /<.*?>/
+  const result = myString.match(myRegex)
+  return result
+}
+
+/*
+Time to pause and test your new regex writing skills. 
+A group of criminals escaped from jail and ran away, but you don't know how many. However, you do know that they stay close together when they are around other people. 
+You are responsible for finding all of the criminals at once.
+
+Here's an example to review how to do this:
+"z"
+"zzzzzz"
+"ABCzzzz"
+"zzzzABC"
+"abczzzzzzzzzzzzzzzzzzzzzabc"
+
+But not match
+""
+"ABC"
+"abcabc"
+
+The regex /z+/ matches the constter z when it appears one or more times in a row. It would find matches in all of the following strings:
+ */
+const findCriminal = (myString) => {
+  const myRegex = /C+/
+  const result = myString.match(myRegex)
+  return result
+}
+
+// First world checked
+const callFirstMatch = (myString) => {
+  const myRegex = /^Cal/
+  return myRegex.test(myString)
+}
+
+// Last world checked
+const callLasttMatch = (myString) => {
+  const myRegex = /one$/
+  return myRegex.test(myString)
+}
+
+// Match upper and lowercase, include _ underscore
+const matchAll = (myString) => {
+  const myRegex = /\w/
+  return myRegex.test(myString)
 }
 
 module.exports = {
@@ -27,9 +109,25 @@ module.exports = {
   match,
   matchAllCase,
   matchDot,
-  vocalsMatch
+  vocalsMatch,
+  vocalsRangeMatch,
+  rangeconsttersNumbersMatch,
+  rangeNegateMatch,
+  matchConsecutive,
+  matchZeroOrMore,
+  lazyMatch,
+  findCriminal,
+  callFirstMatch,
+  callLasttMatch,
+  matchAll
 }
 
 // i = IgnoreCase
 // g = Find more of one result as an array
+// . = Is a wildcard that will match any one character
 // [] = find the specific element
+// - = define a range of characters to match
+// [^] is used to negate
+// + = consecutive
+// * = zero or more
+// ? = is a lazy way to find a match in a spec
--- regex.test.js
+++ regex.test.js
@@ -3,7 +3,17 @@ const {
   match,
   matchAllCase,
   matchDot,
-  vocalsMatch
+  vocalsMatch,
+  vocalsRangeMatch,
+  rangeconsttersNumbersMatch,
+  rangeNegateMatch,
+  matchConsecutive,
+  matchZeroOrMore,
+  lazyMatch,
+  findCriminal,
+  callFirstMatch,
+  callLasttMatch,
+  matchAll
 } = require('./index')
 const { describe, expect, test } = require('@jest/globals')
 
@@ -25,6 +35,7 @@ describe('Function match', () => {
     expect(match(extractStr)).toBe('coding')
   })
 })
+
 describe('Function matchAllCase', () => {
   const string = 'Twinkle, twinkle, little star'
   const expected = ['Twinkle', 'twinkle']
@@ -32,8 +43,9 @@ describe('Function matchAllCase', () => {
     expect(matchAllCase(string)).toEqual(expect.arrayContaining(expected))
   })
 })
+
 describe('Function matchDot', () => {
-  const exampleStr = "Let's have fun with regular expressions!"
+  const exampleStr = "const's have fun with regular expressions!"
   const exampleStr1 = 'The sun is out today.'
   const exampleStr2 = 'Coding is a lot of fun.'
   const exampleStr3 = 'Seven days without a pun makes one weak.'
@@ -66,5 +78,138 @@ describe('Function vocalsMatch', () => {
     'Beware of bUgs in the AbovE cOde; I have only ProveD It correct, nOT tried it.'
   test('Should return 25 vocals', () => {
     expect(vocalsMatch(quoteSample)).toHaveLength(25)
+  })
+})
+
+describe('Function vocalsRangeMatch', () => {
+  const quoteSample = 'thE quiCk BrOWN fox jumpS Over the lazy doG.'
+  test('Should return 35 vocals', () => {
+    expect(vocalsRangeMatch(quoteSample)).toHaveLength(35)
+  })
+  test('Should not have a dot on the array', () => {
+    expect(vocalsRangeMatch(quoteSample)).not.toContain('.')
+  })
+})
+
+describe('Function rangeconsttersNumbersMatch', () => {
+  const quoteSample = 'BluebeRrY 3.141592653s are dElIcious.'
+  test('Should return 17 items', () => {
+    expect(rangeconsttersNumbersMatch(quoteSample)).toHaveLength(17)
+  })
+  test('Should not have a dot on the array', () => {
+    expect(rangeconsttersNumbersMatch(quoteSample)).not.toContain('.')
+  })
+})
+
+describe('Function rangeNegateMatch', () => {
+  const quoteSample = '3 blind mice.'
+  test('Should return 9 items', () => {
+    expect(rangeNegateMatch(quoteSample)).toHaveLength(9)
+  })
+  test('Should not have a number', () => {
+    expect(rangeNegateMatch(quoteSample)).not.toContain('3')
+  })
+  test('Should have a dot', () => {
+    expect(rangeNegateMatch(quoteSample)).toContain('.')
+  })
+})
+
+describe('Function matchConsecutive', () => {
+  const quoteSample = 'Mississippi'
+  test('Should return 2 items', () => {
+    expect(matchConsecutive(quoteSample)).toHaveLength(2)
+  })
+  test('Should be equal to ["ss","ss"]', () => {
+    expect(matchConsecutive(quoteSample)).toEqual(['ss', 'ss'])
+  })
+})
+
+describe('Function matchZeroOrMore', () => {
+  const quoteSample = 'Aaaaaaaaaaaaaaaarrrgh!'
+  const quoteSample2 = "He made a fair move. Screaming about it can't help you."
+  test('Should contain Aaaaaaaaaaaaaaaa', () => {
+    expect(matchZeroOrMore(quoteSample)).toContain('Aaaaaaaaaaaaaaaa')
+  })
+  test('Should not match and return null', () => {
+    expect(matchZeroOrMore(quoteSample2)).toBeNull()
+  })
+})
+
+describe('Function lazyMatch', () => {
+  const text = '<h1>Winter is coming</h1>'
+
+  test('Should return ', () => {
+    expect(lazyMatch(text)).toContain('<h1>')
+  })
+})
+
+describe('Function findCriminal', () => {
+  const text1 = 'P6P2P7P4P5CCCCCP3P1'
+  const text2 = ''
+  const text3 = 'P1P5P4CCCcP2P6P3'
+  const text4 = 'P1P2P3'
+
+  test('Should match CCCCC', () => {
+    expect(findCriminal(text1)).toContain('CCCCC')
+  })
+  test('Should not Match an empty array', () => {
+    expect(findCriminal(text2)).toBeNull()
+  })
+  test('Should Match CCC', () => {
+    expect(findCriminal(text3)).toContain('CCC')
+  })
+  test('Should not Match P1P2P3', () => {
+    expect(findCriminal(text4)).toBeNull()
+  })
+})
+
+describe('Function callFirstMatch', () => {
+  const textTrue = 'Cal and Ricky both like racing.'
+  const textFalse = 'Ricky and Cal both like racing.'
+  const textFalse2 = 'Ca is a element.'
+
+  test('Should find Carl and return True', () => {
+    expect(callFirstMatch(textTrue)).toBeTruthy()
+  })
+  test('Should not find Cal and return False', () => {
+    expect(callFirstMatch(textFalse)).toBeFalsy()
+  })
+  test('Should not find Cal and return False', () => {
+    expect(callFirstMatch(textFalse2)).toBeFalsy()
+  })
+})
+describe('Function callLasttMatch', () => {
+  const textTrue = 'Harry Potter is the choosen one'
+  const textFalse = 'One Pience is real'
+  const textFalse2 = 'I am the last one?'
+  const textFalse3 = 'One'
+
+  test('Should find one and return True', () => {
+    expect(callLasttMatch(textTrue)).toBeTruthy()
+  })
+  test('Should not find one and return False', () => {
+    expect(callLasttMatch(textFalse)).toBeFalsy()
+  })
+  test('Should not find one and return False', () => {
+    expect(callLasttMatch(textFalse2)).toBeFalsy()
+  })
+  test('Should not find one because have an UppcerCase and return false', () => {
+    expect(callLasttMatch(textFalse3)).toBeFalsy()
+  })
+})
+
+describe('Function matchAll', () => {
+  const strings = 'Avada Kadabra'
+  const numbers = '17'
+  const string_underscore = 'I_AM_YOUR_FATHER'
+
+  test('Should find one and return True', () => {
+    expect(matchAll(strings)).toBeTruthy()
+  })
+  test('Should not find one and return False', () => {
+    expect(matchAll(numbers)).toBeTruthy()
+  })
+  test('Should not find one and return False', () => {
+    expect(matchAll(string_underscore)).toBeTruthy()
   })
 })
